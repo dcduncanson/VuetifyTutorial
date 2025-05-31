a5877d505de64d2c385a993e597df9e381d78ab7
Derek <dcduncanson@gmail.com>
2025-05-30 18:02:26 -0600

    vuetify app start

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Sidebar from "@/components/Sidebar.vue";
+</script>
+
+<template>
+  <v-app>
+    <!-- Sidebar-->
+    <Sidebar />
+    <v-main>
+      <!-- Cards -->
+    </v-main>
+  </v-app>
+</template>
